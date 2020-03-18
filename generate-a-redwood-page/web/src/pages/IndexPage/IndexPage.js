be70390015de519b1851431fcd05d521266f9274
ijones16 <jones58ian@gmail.com>
2020-03-18 08:41:38 -0400

    finish redwood routes section

--- generate-a-redwood-page/web/src/pages/IndexPage/IndexPage.js
+++ generate-a-redwood-page/web/src/pages/IndexPage/IndexPage.js
@@ -1,6 +1,15 @@
+import { Link, routes } from '@redwoodjs/router'
+
 const IndexPage = () => {
   return (
     <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to={routes.about()}>About</Link>
+          </li>
+        </ul>
+      </nav>
       <h1>IndexPage</h1>
       <p>hello egghead</p>
     </div>
